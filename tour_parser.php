32d4b81ca779338786df301980a1e6523093a19a
Riyad <riyad.hasanov@gmail.com>
2017-08-05 08:29:04 -0400

    final cleanup

--- tour_parser.php
+++ tour_parser.php
@@ -1,4 +1,48 @@
 <?php
+$text = '<?xml version="1.0"?>
+<TOURS>
+    <TOUR>
+        <Title><![CDATA[Anzac &amp; Egypt Combo Tour]]></Title>
+        <Code>AE-19</Code>
+        <Duration>18</Duration>
+        <Start>Istanbul</Start>
+        <End>Cairo</End>
+        <Inclusions>
+            <![CDATA[<div style="margin: 1px 0px; padding: 1px 0px; border: 0px; outline: 0px; font-size: 14px; vertical-align: baseline; text-align: justify; line-height: 19px; color: rgb(6, 119, 179);">The tour price&nbsp; cover the following services: <b style="margin: 0px; padding: 0px; border: 0px; outline: 0px; vertical-align: baseline; background-color: transparent;">Accommodation</b>; 5, 4&nbsp;and&nbsp;3 star hotels&nbsp;&nbsp;</div>]]>
+        </Inclusions>
+        <DEP DepartureCode="AN-17" Starts="04/19/2015" GBP="1458" EUR="1724" USD="2350" DISCOUNT="15%" />
+        <DEP DepartureCode="AN-18" Starts="04/22/2015" GBP="1558" EUR="1784" USD="2550" DISCOUNT="20%" />
+        <DEP DepartureCode="AN-19" Starts="04/25/2015" GBP="1558" EUR="1784" USD="2550" />
+    </TOUR>
+    <TOUR>
+        <Title><![CDATA[Anzac &amp; Egypt Combo Tour]]></Title>
+        <Code>AE-19</Code>
+        <Duration>18</Duration>
+        <Start>Istanbul</Start>
+        <End>Cairo</End>
+        <Inclusions>
+            <![CDATA[<div style="margin: 1px 0px; padding: 1px 0px; border: 0px; outline: 0px; font-size: 14px; vertical-align: baseline; text-align: justify; line-height: 19px; color: rgb(6, 119, 179);">The tour price&nbsp; cover the following services: <b style="margin: 0px; padding: 0px; border: 0px; outline: 0px; vertical-align: baseline; background-color: transparent;">Accommodation</b>; 5, 4&nbsp;and&nbsp;3 star hotels&nbsp;&nbsp;</div>]]>
+        </Inclusions>
+        <DEP DepartureCode="AN-17" Starts="04/19/2015" GBP="1458" EUR="1724" USD="2350" DISCOUNT="15%" />
+        <DEP DepartureCode="AN-18" Starts="04/22/2015" GBP="1558" EUR="1784" USD="2550" DISCOUNT="20%" />
+        <DEP DepartureCode="AN-19" Starts="04/25/2015" GBP="1558" EUR="1784" USD="1200" />
+    </TOUR>
+
+    <TOUR>
+        <Title><![CDATA[Anzac &amp; Egypt Combo Tour]]></Title>
+        <Code>AE-19</Code>
+        <Duration>18</Duration>
+        <Start>Istanbul</Start>
+        <End>Cairo</End>
+        <Inclusions>
+            <![CDATA[<div style="margin: 1px 0px; padding: 1px 0px; border: 0px; outline: 0px; font-size: 14px; vertical-align: baseline; text-align: justify; line-height: 19px; color: rgb(6, 119, 179);">The tour price&nbsp; cover the following services: <b style="margin: 0px; padding: 0px; border: 0px; outline: 0px; vertical-align: baseline; background-color: transparent;">Accommodation</b>; 5, 4&nbsp;and&nbsp;3 star hotels&nbsp;&nbsp;</div>]]>
+        </Inclusions>
+        <DEP DepartureCode="AN-17" Starts="04/19/2015" GBP="1458" EUR="1724" USD="1200" DISCOUNT="15%" />
+        <DEP DepartureCode="AN-18" Starts="04/22/2015" GBP="1558" EUR="1784" USD="2550" DISCOUNT="20%" />
+        <DEP DepartureCode="AN-19" Starts="04/25/2015" GBP="1558" EUR="1784" USD="1200" />
+    </TOUR>
+</TOURS>';
+
 
 function xmlToCSV($text) {
   $tours = simplexml_load_string($text);
@@ -12,11 +56,12 @@ function xmlToCSV($text) {
       $discount  = $dep->attributes()->DISCOUNT ? $dep->attributes()->DISCOUNT: 0;
       $discount = (int) str_replace('%', '', $discount);
       $discount = (100 - $discount) / 100;
-      echo $discount . '<br>';
       $min_USD = ($dep->attributes()->USD * $discount) < $min_USD ? $dep->attributes()->USD * $discount : $min_USD;
       $min_GBP = ($dep->attributes()->GBP * $discount) < $min_GBP ? $dep->attributes()->GBP * $discount : $min_GBP;
       $min_EUR = ($dep->attributes()->EUR * $discount) < $min_EUR ? $dep->attributes()->EUR * $discount : $min_EUR;
     }
-    echo $tour->Title . '|' . $tour->Code  . '|' . $tour->Duration . '|' . $tour->Inclusions . '|' . $min_USD;
+    echo $tour->Title . '|' . $tour->Code  . '|' . $tour->Duration . '|' . $tour->Inclusions . '|' . $min_USD . '</br>';
   }
 }
+
+xmlToCSV($text);
